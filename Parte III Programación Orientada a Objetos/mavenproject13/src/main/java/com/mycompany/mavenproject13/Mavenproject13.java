38f636172054fd36b66fff92b12e4bc8b7274658
Víctor-vctr <salramirez7616@gmail.com>
2025-03-18 22:53:26 -0500

    ma ejer

---
+++ Parte III Programación Orientada a Objetos/mavenproject13/src/main/java/com/mycompany/mavenproject13/Mavenproject13.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject13;
+
+/**
+ *
+ * @author dev-ops
+ */
+public class Mavenproject13 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
